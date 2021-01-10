ee2a3ec81ebe6b737cd03d2d76b058789d628896
Isaac <isaackomeza@gmail.com>
2021-01-10 16:06:13 +0200

    Controlled Forms

--- src/shared/leaders.js
+++ src/shared/leaders.js
@@ -33,6 +33,6 @@ export const LEADERS = [
       designation: 'Executive Chef',
       abbr: 'EC',
       featured: true,
-      description: 'Award winning three-star Michelin chef with wide International experience having worked closely with whos-who in the culinary world, he specializes in creating mouthwatering Indo-Italian fusion experiences. He says, Put together the cuisines from the two craziest cultures, and you get a winning hit! Amma Mia!'
+      description: 'Award winning three-star Michelin chef with wide International experience having worked closely with whos-who in the culinary world, he specializes in creating Indo-Italian fusion experiences.'
     }
   ];
