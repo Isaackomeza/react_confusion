d7e381f0e6e90ac990f0f8bcb86d9ca9254e2f51
Isaac <isaackomeza@gmail.com>
2021-01-20 11:01:35 +0200

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -6,6 +6,7 @@ import{Button, Modal, ModalHeader, ModalBody,
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -125,7 +126,8 @@ function RenderDish({dish}){
     return(
         <div>
         <Card>    
-            <CardImg width="100%"  src={dish.image} alt={dish.name} />
+            {/* <CardImg width="100%"  src={dish.image} alt={dish.name} /> */}
+            <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
             <CardBody>
               <CardTitle>{dish.name}</CardTitle>
               <CardText>{dish.description}</CardText>
