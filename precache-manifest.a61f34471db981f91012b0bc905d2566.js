440707bc9b42b7cbe0ba1685f6f1e36d97b28794
Isaac <isaackomeza@gmail.com>
2021-01-24 09:21:43 +0200

    Updates

---
+++ precache-manifest.a61f34471db981f91012b0bc905d2566.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd8567d0f7a7867bd7ff78615acf7ac",
+    "url": "/react_confusion/index.html"
+  },
+  {
+    "revision": "8b213ae83a1d1d15b4bf",
+    "url": "/react_confusion/static/css/2.b30d427f.chunk.css"
+  },
+  {
+    "revision": "56e190a1d01cd43f0d33",
+    "url": "/react_confusion/static/css/main.7c7297cb.chunk.css"
+  },
+  {
+    "revision": "8b213ae83a1d1d15b4bf",
+    "url": "/react_confusion/static/js/2.9d179bf1.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/react_confusion/static/js/2.9d179bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56e190a1d01cd43f0d33",
+    "url": "/react_confusion/static/js/main.572262f1.chunk.js"
+  },
+  {
+    "revision": "6927e85aa42ff8d70bb2",
+    "url": "/react_confusion/static/js/runtime-main.b496a0fc.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react_confusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react_confusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react_confusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react_confusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react_confusion/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
